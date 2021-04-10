e71a4ec631bb34310a0ee4926d597ec7151028d3
Moonwalker <abhisk.mhra@gmail.com>
2021-04-10 19:04:27 +0530

    Tanking input from browser and showing on console

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var transButton = document.querySelector("#click-btn");
+var textInput = document.querySelector("#text-input");
 
 transButton.addEventListener("click",function click(){
     console.log("Clicked");
+    console.log("input", textInput.value);
 });
+
+
+
+
+console.log(textInput );
